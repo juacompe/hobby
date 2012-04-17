d77c3ce8ffdb8ccb4f587835face3305535d6293
juacompe <juacompe@gmail.com>
2012-04-17 14:21:05 +0700

    - complete master config

--- buildbot_config/settings.py
+++ buildbot_config/settings.py
@@ -1,6 +1,6 @@
 BRANCH = "develop"
-MASTER_HOST = "buildbot_master_host"
-PROJECT_CODE_URL = "project_dirname"
+MASTER_HOST = "ec2-50-18-139-254.us-west-1.compute.amazonaws.com"
+PROJECT_CODE_URL = "src"
 PROJECT_NAME = "hobby"
-REPOSITORY_URL = "/path/to/repo"
+REPOSITORY_URL = "git://github.com/juacompe/hobby.git"
 
